bfec7ceca97ac8a2c6f562d4def70c71ab28f994
kwonssshyeon <kwonsh4396@gmail.com>
2023-09-17 20:21:13 +0900

    [feat]테스트용 API 구현

---
+++ src/main/java/com/example/Ahi/controller/TestController.java
@@ -0,0 +1,45 @@
+package com.example.Ahi.controller;
+
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(allowedHeaders = "*")
+@RequestMapping
+public class TestController {
+    @GetMapping("/test")
+    public outputDto getTest(){
+        outputDto outputDto = new outputDto();
+        outputDto.output = "GET-확인되었습니다.";
+        return outputDto;
+    }
+    @PostMapping("/test")
+    public outputDto postTest(@RequestBody String input){
+        outputDto outputDto = new outputDto();
+        outputDto.output = input+" POST-확인되었습니다.";
+        return outputDto;
+    }
+
+    @PostMapping("/test/json")
+    public outputDto jsonTest(@RequestBody inputDto inputDto){
+        outputDto outputDto = new outputDto();
+        outputDto.output = inputDto.getInput()+" POST-확인되었습니다.";
+        return outputDto;
+    }
+
+}
+
+
+@Data
+@Getter
+class outputDto{
+    String output;
+}
+
+@Data
+@Getter
+class inputDto{
+    String input;
+}
