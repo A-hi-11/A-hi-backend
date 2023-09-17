41fcb0ca45ebe74394c5af5e10dce22423d916af
kwonssshyeon <kwonsh4396@gmail.com>
2023-09-17 17:53:08 +0900

    initial commit

---
+++ src/main/java/com/example/Ahi/AHiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ahi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AHiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Ahi/AHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ahi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AHiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
