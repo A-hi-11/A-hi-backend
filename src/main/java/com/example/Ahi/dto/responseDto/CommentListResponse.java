359338b6747ecdc361385d12e503ca00515f8b3f
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-18 10:27:14 +0900

    feat: 본인이 쓴 댓글인지 확인하는 로직 추가

--- src/main/java/com/example/Ahi/dto/responseDto/CommentListResponse.java
+++ src/main/java/com/example/Ahi/dto/responseDto/CommentListResponse.java
@@ -3,9 +3,15 @@ package com.example.Ahi.dto.responseDto;
 import com.example.Ahi.domain.Comment;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class CommentListResponse {
-    private List<Comment> comments;
+    private String content;
+    private LocalDateTime create_time;
+    private Long comment_id;
+    private String member_nickname;
+    private String member_profile_img;
+    private boolean isPermissioned;
 }
