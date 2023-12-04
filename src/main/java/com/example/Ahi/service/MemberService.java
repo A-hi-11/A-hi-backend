a256bc328370f7946ec3c0ed7b82c1ffe20d5bf9
minju26 <mjalswn26@gmail.com>
2023-12-04 23:14:59 +0900

    feat: 소셜 로그인 랜덤 비밀번호 생성

--- src/main/java/com/example/Ahi/service/MemberService.java
+++ src/main/java/com/example/Ahi/service/MemberService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -66,5 +67,29 @@ public class MemberService {
         }
 
         return response;
+    }
+
+
+    public class MemberPasswordGenerator {
+
+        private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
+        private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
+        private static final String NUMBER = "0123456789";
+        private static final String SPECIAL_CHARS = "!@#$%^&*()_-+=<>?";
+
+        private static final String PASSWORD_CHARS = CHAR_LOWER + CHAR_UPPER + NUMBER + SPECIAL_CHARS;
+
+        public static String generateRandomPassword(int length) {
+            SecureRandom random = new SecureRandom();
+            StringBuilder password = new StringBuilder();
+
+            for (int i = 0; i < length; i++) {
+                int index = random.nextInt(PASSWORD_CHARS.length());
+                password.append(PASSWORD_CHARS.charAt(index));
+            }
+
+            return password.toString();
+        }
+
     }
 }
